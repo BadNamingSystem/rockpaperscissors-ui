1864b7a5c41e5f0de293a676b8c4d7ebd529f48e
BadNamingSystem <vlad.claici@yahoo.com>
2025-07-19 21:50:16 +0300

    almost done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const choices = ["rock", "paper", "scissors"];
-document.getElementById("rock")
-document.getElementById("paper")
-document.getElementById("scissors")
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+const reset = document.getElementById("resetGame")
+const playerScoreDisplay = document.getElementById("playerScoreDisplay")
+const computerScoreDisplay = document.getElementById("computerScoreDisplay")
+const drawCountDisplay = document.getElementById("drawCountDisplay")
+const resultDisplay = document.getElementById("resultDisplay")
+const roundInfo = document.getElementById("roundInfo")
+
+rock.addEventListener("click", () => playRound("rock"))
+paper.addEventListener("click", () => playRound("paper"))
+scissors.addEventListener("click", () => playRound("scissors"))
+reset.addEventListener("click", () => resetGame())
+
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
@@ -22,11 +34,30 @@ function playRound(playerSelect) {
         result = `You win because ${playerSelect} beats ${computerChoice}`
         playerScore++
     } else {
-        console.log(`You lose because ${computerChoice} beats ${playerSelect}`)
+        result = `You lose because ${computerChoice} beats ${playerSelect}`
         computerScore++
     }
-    console.log(`You won ${playerScore} times and computer won ${computerScore} times.`)
+    resultDisplay.textContent = result
+    roundInfo.textContent = `You chose ${playerSelect} and computer chose ${computerChoice}`
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`
+    drawCountDisplay.textContent = `Draw Count: ${drawScore}`
 }
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    roundInfo.textContent = "Click the buttons"
+    resultDisplay.textContent = "Good luck!"
+    playerScoreDisplay.textContent = `Player Score: 0`
+    computerScoreDisplay.textContent = `Computer Score: 0`
+    drawCountDisplay.textContent = `Draw Count: 0`
+}
+
+
+
+
 
 
 
